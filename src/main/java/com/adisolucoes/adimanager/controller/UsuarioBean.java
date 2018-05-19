da82acec72094d7d53c4dd8c9a8297e59a494935
ADI Soluções <ADI Solues@DESKTOP-JHC53G1>
2018-05-19 09:04:32 -0300

    Alteração do Template e Consulta de Usuários

--- src/main/java/com/adisolucoes/adimanager/controller/UsuarioBean.java
+++ src/main/java/com/adisolucoes/adimanager/controller/UsuarioBean.java
@@ -1,20 +1,14 @@
 package com.adisolucoes.adimanager.controller;
 
 import com.adisolucoes.adimanager.dao.UsuarioDAO;
-import com.adisolucoes.adimanager.enumerations.Sexo;
 import com.adisolucoes.adimanager.enumerations.TipoUsuario;
-import com.adisolucoes.adimanager.enumerations.UF;
 import com.adisolucoes.adimanager.exceptions.ErroBancoDadosException;
-import com.adisolucoes.adimanager.model.Endereco;
-import com.adisolucoes.adimanager.model.Pessoa;
 import com.adisolucoes.adimanager.model.Usuario;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -31,93 +25,71 @@ public class UsuarioBean implements Serializable {
 
     @Inject
     private UsuarioDAO usuarioDAO;
+    @Inject
+    private Usuario usuario;
+
+    private String nome;
+    private TipoUsuario tipoUsuario;
 
     private List<Usuario> usuarioFiltrados;
 
-    @PostConstruct
-    public void init() {
+    public List<Usuario> pesquisar() {
         try {
-            usuarioFiltrados = new ArrayList<Usuario>();
+            usuarioFiltrados = new ArrayList<>();
+            usuarioFiltrados = usuarioDAO.buscarPorNomeTipoUsuario(nome, tipoUsuario);
+            nome = "";
+            tipoUsuario = null;
+        } catch (ErroBancoDadosException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+        return usuarioFiltrados;
+    }
 
-            Endereco endereco = new Endereco();
-            endereco.setBairro("Sao Paulo");
-            endereco.setCep("64027.080");
-            endereco.setCidade("Barueri");
-            endereco.setLogradouro("Quadra-30 Lote-21 Casa-B");
-            endereco.setNumero("456");
-            endereco.setUf(UF.DF);
-
-            Pessoa pessoa = new Pessoa();
-            pessoa.setCpfCnpj("098764343");
-            pessoa.setNome("Jonathan Silva Sousa");
-            pessoa.setDataAtualizacao(new Date());
-            pessoa.setDataCadastro(new Date());
-            pessoa.setDataNascimento(new Date());
-            pessoa.setEmail("marioadi157@gmail");
-            pessoa.setEndereco(endereco);
-            pessoa.setNacionalidade("Brasileiro");
-            pessoa.setObservacao("Referenciada");
-            pessoa.setSexo(Sexo.FEMININO);
-            pessoa.setTelefone("0101001");
-
-            Usuario usuario = new Usuario();
-            usuario.setDataAtualizacao(new Date());
-            usuario.setDataCadastro(new Date());
-            usuario.setDataUltimoAcesso(new Date());
-            usuario.setLogin("JUnior teste");
-            usuario.setPessoa(pessoa);
-            usuario.setSenha("22323");
-            usuario.setStatus(true);
-            usuario.setTipoUsuario(TipoUsuario.MASTER);
-            usuario.setPessoa(pessoa);
-
-            Endereco endereco2 = new Endereco();
-            endereco2.setBairro("Sao Paulo");
-            endereco2.setCep("64027.080");
-            endereco2.setCidade("Barueri");
-            endereco2.setLogradouro("Quadra-30 Lote-21 Casa-B");
-            endereco2.setNumero("456");
-            endereco2.setUf(UF.DF);
-
-            Pessoa pessoa2 = new Pessoa();
-            pessoa2.setCpfCnpj("098764343");
-            pessoa2.setNome("Jonathan Silva Sousa");
-            pessoa2.setDataAtualizacao(new Date());
-            pessoa2.setDataCadastro(new Date());
-            pessoa2.setDataNascimento(new Date());
-            pessoa2.setEmail("marioadi157@gmail");
-            pessoa2.setEndereco(endereco2);
-            pessoa2.setNacionalidade("Brasileiro");
-            pessoa2.setObservacao("Referenciada");
-            pessoa2.setSexo(Sexo.FEMININO);
-            pessoa2.setTelefone("0101001");
-
-            Usuario usuario2 = new Usuario();
-            usuario2.setDataAtualizacao(new Date());
-            usuario2.setDataCadastro(new Date());
-            usuario2.setDataUltimoAcesso(new Date());
-            usuario2.setLogin("JUnior teste");
-            usuario2.setPessoa(pessoa2);
-            usuario2.setSenha("22323");
-            usuario2.setStatus(true);
-            usuario2.setTipoUsuario(TipoUsuario.MASTER);
-            usuario2.setPessoa(pessoa2);
-
-            usuarioFiltrados.add(usuario);
-            usuarioFiltrados.add(usuario2);
-            usuarioDAO.salvar(usuario);
-            System.out.println("Cadastrado");
+    public void preencherTabela() {
+        try {
+            usuarioFiltrados = usuarioDAO.listarTodos();
         } catch (ErroBancoDadosException ex) {
             LOG.log(Level.SEVERE, null, ex);
         }
     }
 
+    public TipoUsuario[] getTiposUsuario() {
+        return TipoUsuario.values();
+    }
+
     public List<Usuario> getUsuarioFiltrados() {
+        if (usuarioFiltrados == null) {
+            preencherTabela();
+        }
         return usuarioFiltrados;
     }
 
     public void setUsuarioFiltrados(List<Usuario> usuarioFiltrados) {
         this.usuarioFiltrados = usuarioFiltrados;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
     }
 
 }
--- src/main/java/com/adisolucoes/adimanager/dao/UsuarioDAO.java
+++ src/main/java/com/adisolucoes/adimanager/dao/UsuarioDAO.java
@@ -1,7 +1,16 @@
 package com.adisolucoes.adimanager.dao;
 
+import com.adisolucoes.adimanager.enumerations.TipoUsuario;
+import com.adisolucoes.adimanager.exceptions.ErroBancoDadosException;
 import com.adisolucoes.adimanager.model.Usuario;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -9,8 +18,33 @@ import java.io.Serializable;
  */
 public class UsuarioDAO extends DAO<Usuario> implements Serializable {
 
+    private static final Logger LOG = Logger.getLogger(UsuarioDAO.class.getName());
+
     public UsuarioDAO() {
         super(Usuario.class);
     }
 
+    public List<Usuario> buscarPorNomeTipoUsuario(String nome, TipoUsuario tipoUsuario) throws ErroBancoDadosException {
+        try {
+            List<Usuario> listaUsuarios = new ArrayList<>();
+            TypedQuery<Usuario> query;
+            String sql = "SELECT u FROM Usuario u WHERE u.pessoa.nome LIKE :nome";
+            if (tipoUsuario != null) {
+                sql += " AND u.tipoUsuario= :tipoUsuario";
+            }
+            sql += " ORDER BY u.pessoa.nome";
+            query = manager.createQuery(sql, Usuario.class);
+            query.setParameter("nome", "%" + nome + "%");
+            if (tipoUsuario != null) {
+                query.setParameter("tipoUsuario", tipoUsuario);
+            }
+            listaUsuarios = query.getResultList();
+            return listaUsuarios;
+        } catch (NonUniqueResultException | IllegalArgumentException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            throw new ErroBancoDadosException(ex.getMessage());
+        } catch (NoResultException ex) {
+            return null;
+        }
+    }
 }
