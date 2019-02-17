d50f289ea9022cfe0ad6fb5b449f4b3e6824c52f
jonat <jonat@DESKTOP-RHGVL0U>
2019-02-17 17:11:46 -0300

    Cadastro, exclusão e edição de metas Ajuste no componente selectBoolean do primefaces

--- src/main/java/com/adisolucoes/adimanager/controller/MetaBean.java
+++ src/main/java/com/adisolucoes/adimanager/controller/MetaBean.java
@@ -2,11 +2,14 @@ package com.adisolucoes.adimanager.controller;
 
 import com.adisolucoes.adimanager.dao.MetaDAO;
 import com.adisolucoes.adimanager.enumerations.TipoPrioridadeMeta;
+import com.adisolucoes.adimanager.exceptions.ErroBancoDadosException;
 import com.adisolucoes.adimanager.filtros.MetaFiltro;
 import com.adisolucoes.adimanager.model.LazyBean;
 import com.adisolucoes.adimanager.model.Meta;
+import com.adisolucoes.adimanager.util.jsf.FacesUtils;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -25,19 +28,70 @@ public class MetaBean implements Serializable {
     @Inject
     private MetaDAO metaDAO;
 
+    private int codigo;
     private Meta meta;
+    private Meta metaSelecionada;
     private MetaFiltro metaFiltro;
     private boolean buscaAvancada;
     private List<Meta> metas;
     private LazyBean<Meta> modelo;
 
     public MetaBean() {
-        meta = new Meta();
-        metaFiltro = new MetaFiltro();
+        limparForm();
     }
 
     public void pesquisarLazy() {
         modelo = new LazyBean<Meta>(metaDAO, metaFiltro);
+    }
+
+    public void limparForm() {
+        meta = new Meta();
+        metaFiltro = new MetaFiltro();
+    }
+
+    public void salvar() {
+        try {
+            if (meta != null) {
+                if (meta.getId() == 0) {
+                    metaDAO.salvar(meta);
+                    limparForm();
+                    FacesUtils.showFacesMessage("Meta salva com sucesso!", 2);
+                } else {
+                    metaDAO.atualizar(meta);
+                    FacesUtils.showFacesMessage("Meta atualizada com sucesso!", 2);
+                }
+            }
+        } catch (ErroBancoDadosException ex) {
+            FacesUtils.showFacesMessage("Houve um erro no banco de dados, consulte o suporte!", 1);
+            LOG.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void excluirMeta() {
+        try {
+            if (metaSelecionada != null) {
+                if (metaSelecionada.isConcluida()) {
+                    FacesUtils.showFacesMessage("Erro, metas concluídas não podem ser apagadas!", 1);
+                } else {
+                    metaDAO.excluir(metaSelecionada.getId());
+                    modelo = null;
+                    FacesUtils.showFacesMessage("Meta excluída com sucesso!", 2);
+                }
+            }
+        } catch (ErroBancoDadosException ex) {
+            FacesUtils.showFacesMessage("Erro no banco de dados, contate o suporte!", 1);
+            LOG.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void buscarMeta() {
+        try {
+            codigo = codigo / 483957299;
+            meta = metaDAO.buscarPorId(new Long(codigo));
+        } catch (ErroBancoDadosException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            FacesUtils.showFacesMessage("Erro ao recuperar meta!", 1);
+        }
     }
 
     public Meta getMeta() {
@@ -64,7 +118,7 @@ public class MetaBean implements Serializable {
         this.metas = metas;
     }
 
-    public TipoPrioridadeMeta[] getPrioridadeMetas() {
+    public TipoPrioridadeMeta[] getPrioridadesMeta() {
         return TipoPrioridadeMeta.values();
     }
 
@@ -85,6 +139,22 @@ public class MetaBean implements Serializable {
 
     public void setModelo(LazyBean<Meta> modelo) {
         this.modelo = modelo;
+    }
+
+    public Meta getMetaSelecionada() {
+        return metaSelecionada;
+    }
+
+    public void setMetaSelecionada(Meta metaSelecionada) {
+        this.metaSelecionada = metaSelecionada;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
 }
--- src/main/java/com/adisolucoes/adimanager/enumerations/TipoUsuario.java
+++ src/main/java/com/adisolucoes/adimanager/enumerations/TipoUsuario.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 public enum TipoUsuario implements Serializable {
     MASTER("Master", new String[]{"consultas/consultaUsuarios.xhtml", "cadastros/cadastroUsuarios.xhtml", "consultas/consultaFerramentas.xhtml", "consultas/consultaEmpresas.xhtml", "consultas/consultaClientes.xhtml",
         "cadastros/cadastroClientes.xhtml", "cadastros/cadastroEmpresa.xhtml", "consultas/consultaIndicacoes.xhtml", "cadastros/cadastroIndicacoes.xhtml", "cadastros/cadastroNotificacao.xhtml", "consultas/consultaNotificacoes.xhtml",
-        "consultas/consultaMetas.xhtml"}),
+        "consultas/consultaMetas.xhtml", "cadastros/cadastroMetas.xhtml"}),
     FUNCIONARIO("Funcionario", new String[]{}),
     CLIENTE("Cliente", new String[]{});
 
